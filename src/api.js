356b76401d0ff138a00b24bb39b03bdb03a020d7
vinhyan <vnhan1@myseneca.ca>
2023-12-11 15:57:58 -0500

    added update, delete and file upload function

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://localhost:8080';
+const apiUrl = process.env.API_URL;
 
 /**
  * Given an authenticated user, request all fragments for this user from the
@@ -17,6 +17,8 @@ export async function getUserFragments(user, expand = false) {
     const res = await fetch(
       `${apiUrl}/v1/fragments${expand ? '?expand=1' : ''}`,
       {
+        method: 'GET',
+        // mode: 'cors',
         // Generate headers with the proper Authorization bearer token to pass
         headers: user.authorizationHeaders(),
       }
@@ -41,6 +43,8 @@ export async function getFragmentDataById(user, fragmentId, ext) {
     const res = await fetch(
       `${apiUrl}/v1/fragments/${fragmentId}${ext ? `.${ext}` : ''}`,
       {
+        method: 'GET',
+        // mode: 'cors',
         // Generate headers with the proper Authorization bearer token to pass
         headers: user.authorizationHeaders(),
       }
@@ -52,12 +56,20 @@ export async function getFragmentDataById(user, fragmentId, ext) {
     console.log('Got user fragments by Id res', res);
     if (res.headers.get('Content-Type').includes('application/json')) {
       const data = await res.json();
-      console.log('*** data', data);
+      // console.log('*** data', data);
       return JSON.stringify(data);
-    } else {
-      // console.log('res.text()', await res.text());
-      return await res.text();
     }
+
+    return res;
+    // } else if (res.headers.get('Content-Type').includes('image')) {
+    //   // const objUrl = URL.createObjectURL(await res.blob());
+    //   // return objUrl;
+    //   return res;
+    // } else {
+    //   // console.log('res.text()', await res.text());
+    //   // return await res.text();
+    //   return res;
+    // }
   } catch (err) {
     console.error(`Unable to call GET ${apiUrl}/v1/fragments/${fragmentId}`, {
       err,
@@ -71,6 +83,8 @@ export async function getFragmentById(user, fragmentId) {
   console.log('Getting fragment by id info...');
   try {
     const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}/info`, {
+      method: 'GET',
+      // mode: 'no-cors',
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(),
     });
@@ -91,16 +105,16 @@ export async function getFragmentById(user, fragmentId) {
 }
 
 // POST NEW FRAGMENT
-export async function submitFragment(user, fragment, dataType) {
+export async function submitFragment(user, fragmentData, dataType) {
   console.log('Submitting fragment data...');
   console.log('apiUrl', apiUrl);
   console.log('dataType', dataType);
-  console.log('fragment', fragment);
+  console.log('fragmentData', fragmentData);
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
       method: 'POST',
       headers: user.authorizationHeaders(dataType),
-      body: fragment,
+      body: fragmentData,
     });
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
@@ -112,3 +126,72 @@ export async function submitFragment(user, fragment, dataType) {
     console.error('Unable to call POST /v1/fragment', { err });
   }
 }
+
+// PUT UPDATE FRAGMENT
+export async function updateFragmentData(
+  user,
+  fragmentId,
+  fragmentData,
+  dataType
+) {
+  console.log('Updating fragment data...');
+  console.log('apiUrl', apiUrl);
+  console.log('dataType', dataType);
+  console.log('fragmentData', fragmentData);
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}`, {
+      method: 'PUT',
+      headers: user.authorizationHeaders(dataType),
+      body: fragmentData,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Updated fragment data', { data });
+    return data;
+  } catch (err) {
+    console.error('Unable to call PUT /v1/fragment', { err });
+  }
+}
+
+// DELETE FRAGMENT
+export async function deleteFragmentData(user, fragmentId) {
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}`, {
+      method: 'DELETE',
+      headers: user.authorizationHeaders(),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    // const data = await res.json();
+    console.log('Deleted fragment res', { res });
+    return res;
+  } catch (err) {
+    console.error('Unable to call DELETE /v1/fragment', { err });
+  }
+}
+
+// helper function:
+const extToTypeConvert = (ext) => {
+  switch (ext) {
+    case 'json':
+      return 'application/json';
+    case 'jpg':
+    case 'jpeg':
+      return 'image/jpeg';
+    case 'png':
+      return 'image/png';
+    case 'gif':
+      return 'image/gif';
+    case 'webp':
+      return 'image/webp';
+    case 'md':
+      return 'text/markdown';
+    case 'html':
+      return 'text/html';
+    default:
+      return 'text/plain';
+  }
+};
--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import {
   submitFragment,
   getFragmentDataById,
   getFragmentById,
-  s,
+  updateFragmentData,
+  deleteFragmentData,
 } from './api';
 
 //test onclick`
@@ -17,8 +18,10 @@ async function init() {
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
   const dataSubmit = document.querySelector('#data-submit');
+  const fileDataSubmit = document.querySelector('#file-data-submit');
   const data = document.querySelector('#data');
   const dataTypeSelect = document.querySelector('#data-type');
+  // const fileDataTypeSelect = document.querySelector('#file-data-type');
   const getData = document.querySelector('#get-data');
   const expandCheckbox = document.querySelector('#expand');
   // const fragmentIdList = document.querySelector('#fragment-id-list');
@@ -26,6 +29,8 @@ async function init() {
   // const getDetailBtn = document.querySelector('#get-detail');
   const fragmentTable = document.querySelector('#fragment-table');
   const fragmentTableBody = document.querySelector('#fragment-table tbody');
+  const updateDataSubmit = document.querySelector('#update-data-submit');
+  const deleteDataSubmit = document.querySelector('#delete-data-submit');
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
@@ -80,22 +85,90 @@ async function init() {
     }
   };
 
+  // Submission Tabs config
+  const tabs = document.querySelectorAll('.tabs li');
+  const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach((item) => item.classList.remove('is-active'));
+      tab.classList.add('is-active');
+
+      const target = tab.dataset.target;
+      tabContentBoxes.forEach((box) => {
+        if (box.getAttribute('id') === target) {
+          box.classList.remove('is-hidden');
+        } else {
+          box.classList.add('is-hidden');
+        }
+      });
+    });
+  });
+
+  // Handle TEXT SUBMIT button
   dataSubmit.onclick = async (e) => {
     const selectedDataType = dataTypeSelect.value;
     console.log('Submitting data...');
-    // console.log(data.value);
-    // console.log('selectedDataType', selectedDataType);
+
     const res = await submitFragment(user, data.value, selectedDataType);
     console.log('>>> res', res);
     if (!res) {
       alert(
         'Error! Your data was not submitted. Please make sure data type is supported'
       );
+    } else {
+      alert('Text submitted successfully!');
+      // location.reload();
     }
+  };
+  const inputFile = document.querySelector('#user-file-upload');
 
-    // init();
+  // disable submit button until a file is selected
+  fileDataSubmit.disabled = true;
+
+  // When a file is selected, show the file name. If not disable the submit button
+  inputFile.onchange = (e) => {
+    // const input = e.target;
+    const fileNameDisplay = document.querySelector('.file-name');
+
+    if (inputFile.files[0]) {
+      fileDataSubmit.disabled = false;
+      const fileName = inputFile.files[0].name;
+      fileNameDisplay.innerText = fileName;
+      fileNameDisplay.classList.remove('has-text-danger');
+    } else {
+      fileDataSubmit.disabled = true;
+      fileNameDisplay.innerText = 'No file selected';
+      fileNameDisplay.classList.add('has-text-danger');
+    }
   };
 
+  // Handle FILE SUBMIT button
+  fileDataSubmit.onclick = async (e) => {
+    console.log('Submitting file data...');
+
+    const reader = new FileReader();
+    reader.onload = async function () {
+      const data = this.result;
+
+      const res = await submitFragment(user, data, inputFile.files[0].type);
+      console.log(res);
+    };
+    if (inputFile.files.length > 0) {
+      reader.readAsArrayBuffer(inputFile.files[0]);
+    }
+
+    if (!res) {
+      alert(
+        'Error! Your data was not submitted. Please make sure data type is supported'
+      );
+    } else {
+      alert('File submitted successfully!');
+      // location.reload();
+    }
+  };
+
+  // Handle RETRIEVE button
   getData.onclick = async (e) => {
     console.log('Getting data...');
     res = await getUserFragments(user, expandCheckbox.checked);
@@ -117,6 +190,8 @@ async function init() {
             .map((fragment) => {
               // let createdDate = new Date(fragment.created);
               let fragmentId = expandCheckbox.checked ? fragment.id : fragment;
+              let fragmentType = fragment.type;
+              console.log('fragmentType', fragmentType);
               return `<tr>
                   <td >
                     <span><span class="has-text-weight-bold">ID:</span> ${fragmentId}</span><br/>
@@ -136,24 +211,41 @@ async function init() {
                     !expandCheckbox.checked
                       ? '<em>Not Available</em>'
                       : `<div class="select is-rounded">
-                      <select name="extension-for-${fragmentId}" id="extension-for-${fragmentId}" ${
-                          fragment.type !== 'text/markdown' ? 'disabled' : ''
-                        }>
+                      <select name="extension-for-${fragmentId}" id="extension-for-${fragmentId}">
                         <option value="">default</option>
+                        <option value="txt">.txt</option>
+                        <option value="md">.md</option>
                         <option value="html">.html</option>
-                        
+                        <option value="json">.json</option>
+                        <option value="png">.png</option>
+                        <option value="jpg">.jpg</option>
+                        <option value="webp">.webp</option>
+                        <option value="gif">.gif</option>
                       </select>
                     </div>`
                   }
                   </td>
-                  <td class="is-vcentered" id=${fragmentId}>
+                  ${
+                    expandCheckbox.checked
+                      ? ` 
+                  <td class="is-vcentered" id=${fragmentId} data-fragment-type=${fragmentType}>
                     <button class="button get-detail js-modal-trigger" data-target="modal-view-data">
-                      View Data
+                      View
+                    </button>
+                    <button class="button has-background-link-light has-text-link	js-edit-modal-trigger" data-target="modal-edit-info">
+                      Edit
                     </button>
                     <button class="button is-info is-light js-view-info-modal-trigger" data-target="modal-view-info">
-                      Info
+                      More
                     </button>
-                  </td>
+                     <button class="button has-background-danger-light has-text-danger js-delete-modal-trigger" data-target="modal-delete">
+                      Delete
+                    </button>
+                    
+                  </td>`
+                      : '<td></td>'
+                  }
+                 
                 </tr>`;
             })
             .join('')
@@ -192,14 +284,53 @@ async function init() {
         $trigger.addEventListener('click', async (e) => {
           const id = $trigger.parentNode.id;
 
-          const extSelect =
-            document.querySelector(`#extension-for-${id}`) || false;
-          let res = await getFragmentDataById(user, id, extSelect.value);
-          const fragmentDataDisplay = document.querySelector(
-            '#fragment-data-display'
-          );
-          fragmentDataDisplay.innerText = res;
-          openModal($target);
+          try {
+            const extSelect =
+              document.querySelector(`#extension-for-${id}`) || false;
+            console.log('extSelect', extSelect.value);
+
+            let res = await getFragmentDataById(user, id, extSelect.value);
+
+            if (!res) {
+              alert('Error! Extension type is not supported for this fragment');
+              return;
+            }
+
+            // console.log('res.body', res.body);
+            const fragmentDataImg = document.querySelector(
+              '#fragment-data-image'
+            );
+
+            const fragmentDataDisplay = document.querySelector(
+              '#fragment-data-display'
+            );
+            // console.log('res', res);
+            const fragmentType = $trigger.parentNode.dataset.fragmentType;
+
+            if (fragmentType.includes('image')) {
+              fragmentDataDisplay.innerText = '';
+              fragmentDataDisplay.hidden = true;
+              fragmentDataImg.hidden = false;
+
+              const resBlob = await res.blob();
+
+              fragmentDataImg.src = URL.createObjectURL(resBlob);
+            } else if (fragmentType.includes('text')) {
+              fragmentDataImg.src = '';
+              fragmentDataImg.hidden = true;
+              fragmentDataDisplay.hidden = false;
+              fragmentDataDisplay.innerText = await res.text();
+            } else if (fragmentType.includes('json')) {
+              fragmentDataImg.src = '';
+              fragmentDataImg.hidden = true;
+              fragmentDataDisplay.hidden = false;
+              fragmentDataDisplay.innerText = res;
+            }
+
+            openModal($target);
+          } catch (err) {
+            console.log(err);
+          }
         });
       }
     );
@@ -300,6 +431,168 @@ async function init() {
         closeAllModals();
       }
     });
+
+    // MODAL FOR UPDATE DATA
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-edit-modal-trigger') || []).forEach(
+      ($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', async (e) => {
+          const updatedData = document.querySelector('#update-data');
+          const updatedDataType = document.querySelector('#update-data-type');
+          const updateFileSection = document.querySelector(
+            '#update-file-section'
+          );
+          const updateTextSection = document.querySelector(
+            '#update-text-section'
+          );
+
+          const fragmentType = $trigger.parentNode.dataset.fragmentType;
+
+          console.log('fragmentType', fragmentType);
+
+          if (fragmentType.includes('image')) {
+            console.log('its an image');
+            console.log(updateFileSection);
+            updateFileSection.style.display = 'block';
+            updateTextSection.style.display = 'none';
+          } else {
+            updateFileSection.style.display = 'none';
+            updateTextSection.style.display = 'block';
+          }
+
+          const fragmentId = $trigger.parentNode.id;
+
+          updateDataSubmit.onclick = async (e) => {
+            console.log('Updating data...');
+
+            if (fragmentType.includes('image')) {
+              const updateInputFile = document.querySelector(
+                '#update-file-upload'
+              );
+
+              const reader = new FileReader();
+              reader.onload = async function () {
+                const data = this.result;
+
+                const res = await updateFragmentData(
+                  user,
+                  fragmentId,
+                  data,
+                  updateInputFile.files[0].type
+                );
+                console.log(res);
+              };
+              if (updateInputFile.files.length > 0) {
+                reader.readAsArrayBuffer(updateInputFile.files[0]);
+              }
+
+              if (!res) {
+                alert(
+                  'Error! Your data was not submitted. Please make sure data type is supported'
+                );
+              }
+            } else {
+              console.log(
+                {
+                  user: user,
+                  fragmentId: fragmentId,
+                  updatedData: updatedData.value,
+                  updatedDataType: updatedDataType.value,
+                },
+                '>>> updateDataSubmit'
+              );
+              const res = await updateFragmentData(
+                user,
+                fragmentId,
+                updatedData.value,
+                updatedDataType.value
+              );
+              if (!res) {
+                alert(
+                  'Error! Your data was not submitted. Please make sure data type is not changed'
+                );
+              }
+              updatedData.value = '';
+            }
+          };
+
+          openModal($target);
+        });
+      }
+    );
+
+    // Add a click event on various child elements to close the parent modal
+    (
+      document.querySelectorAll(
+        '.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button'
+      ) || []
+    ).forEach(($close) => {
+      const $target = $close.closest('.modal');
+
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      if (event.code === 'Escape') {
+        closeAllModals();
+      }
+    });
+
+    // MODAL FOR DELETE
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-delete-modal-trigger') || []).forEach(
+      ($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', async (e) => {
+          const fragmentId = $trigger.parentNode.id;
+
+          deleteDataSubmit.onclick = async (e) => {
+            console.log('Deleteing data...');
+            console.log(
+              {
+                user: user,
+                fragmentId: fragmentId,
+              },
+              '>>> deleteDataSubmit'
+            );
+            const res = await deleteFragmentData(user, fragmentId);
+
+            if (!res) {
+              alert('Error! Your fragment was not deleted. Please try again');
+            } else {
+              alert('Fragment deleted successfully!');
+              location.reload();
+            }
+          };
+
+          openModal($target);
+        });
+      }
+    );
   };
 }
 
